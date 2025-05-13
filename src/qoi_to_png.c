36db56397267693f294a139dd187faf2582f11af
mate02102003 <mate02102003@gmail.com>
2025-05-13 21:49:02 +0200

    added propper flag support

--- src/qoi_to_png.c
+++ src/qoi_to_png.c
@@ -5,11 +5,37 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../thirdparty/stb_image_write.h"
 
+void usage(FILE *stream)
+{
+    fprintf(stream, "Usage: ./qoi_to_png [OPTIONS]\n");
+    fprintf(stream, "OPTIONS:\n");
+    flag_print_options(stream);
+}
+
 int main(int argc, char **argv) {
+    bool *help = flag_bool("help", false, "Print this help to stdout and exit with 0");
     char **input_file = flag_str("input-image", NULL, "Input qoi image path to convert to png (MANDATORY)");
     char **output_file = flag_str("output-image", NULL, "Output png image path (MANDATORY)");
 
     if (!flag_parse(argc, argv)) {
+        usage(stderr);
+        flag_print_error(stderr);
+        return 1;
+    }
+
+    if (*help) {
+        usage(stdout);
+        exit(0);
+    }
+
+    if (*input_file == NULL) {
+        usage(stderr);
+        fprintf(stderr, "ERROR: No -%s was provided\n", flag_name(input_file));
+        return 1;
+    }
+    if (*output_file == NULL) {
+        usage(stderr);
+        fprintf(stderr, "ERROR: No -%s was provided\n", flag_name(output_file));
         return 1;
     }
 
